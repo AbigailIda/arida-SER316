45297e1976c64d6f1f8dbd0a0ef47b0b5f277edd
Abigail Ida <arida@asu.edu>
2019-04-10 00:50:50 -0400

    violation fixes in main

--- src/main/java/Course.java
+++ src/main/java/Course.java
@@ -18,19 +18,19 @@ public class Course {
     private String Name; // course name
 
 
-    public Course(String name) { 
+    public Course(String name){ 
         this.SetName(name);
     }
 
-    public String GetName() {
+    public String GetName(){
         return Name;
     }
 
-    public void SetName(String name) {
+    public void SetName(String name){
         this.Name = name;
     }
     
-    //@javadoc this method prints course stats
+    /**@javadoc this method prints course stats */
     public void printCourseStats() {
         ArrayList<Integer> values = new ArrayList<Integer>(points.values());
 
@@ -39,56 +39,49 @@ public class Course {
     }
     
     
-    // @javadoc REACH at least 95% Code coverage (assign 3)
+    /** @javadoc REACH at least 95% Code coverage (assign 3)
     // method to draw node graph for
     // should throw null pointer for empty points member
     // negative points should be ignored
     // max value and min value should be removed - (if doubles then only the first occurrence)
-    // if just one or two values no values will be omitted
+    // if just one or two values no values will be omitted */
     public double calculateAverageWithoutMinWithoutMax() throws NullPointerException {
         ArrayList<Integer> collection = new ArrayList<Integer>(points.values());
         
-            int counter = 0;
-            int min = Integer.MAX_VALUE;
-            int max = Integer.MIN_VALUE;
-            if(collection.size() == 1)
-            {
-                return collection.get(0);
-            }
-            else if(collection.size() == 2 )
-            {
-                return (double)(collection.get(0) + collection.get(1))/2;
-            }
-            else 
-            {
-                int allPoints = 0;
-                for(int point: collection)
-                {
-                    if (point >= 0) {
-                        
-                        counter = counter++;
-                        if (point < min)
-                        {
-                            min = point;
-                        }
-                        if (point > max)
-                        {
-                            max = point;
-                        }
-                        allPoints = allPoints + point;
+        int counter = 0;
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+        if(collection.size() == 1){
+            return collection.get(0);
+        }
+        else if(collection.size() == 2 ){
+            return (double)(collection.get(0) + collection.get(1))/2;
+        }
+        else{
+            int allPoints = 0;
+            for(int point: collection){
+                if (point >= 0){
+                    
+                    counter = counter++;
+                    if (point < min){
+                        min = point;
                     }
-            }
+                    if (point > max){
+                        max = point;
+                    }
+                    allPoints = allPoints + point;
+                }
             
+            }         
             int totalPoints = allPoints-max-min;
-                return totalPoints/(double)(counter-1); 
-
+            return totalPoints/(double)(counter-1); 
         }
     }
     
     // REACH at least 95% Code coverage  (assign 3)
     // if student with the name (asurite member) is not yet included student needs to be added to student list 
     // sets points for a student 
-    public void set_points(String name, int points)  {
+    public void set_points(String name, int points){
     	System.out.println(points);
         this.points.put(name, points);
     }
@@ -97,32 +90,32 @@ public class Course {
     // REACH at least 95% Code coverage  (assign 3)
     // Students should only be added when they are not yet in the course (names (asurite member) needs to be unique)
     ArrayList<Student> students  = new ArrayList<Student>();
-    public boolean addStudent(Student s) {
+    public boolean addStudent(Student s){
         students.add(s);
         points.put(s.getAsurite(), -1);
         return true;
     }
 
 
-    public HashMap<String, Integer> GetPoints() {
+    public HashMap<String, Integer> GetPoints(){
         return points;
     }
     
 
-    public int getStudent_Points(String student) {
+    public int getStudent_Points(String student){
         return points.get(student);
     }
     
-    public int getStudent_Points(Student student) {
+    public int getStudent_Points(Student student){
         return points.get(student.getAsurite());
     }
 
     
-    public ArrayList<Student> getStudents() {
+    public ArrayList<Student> getStudents(){
         return students;
     }
     
-    public HashMap<String, Integer> countOccurencesLetterGrades() throws NullPointerException {
+    public HashMap<String, Integer> countOccurencesLetterGrades() throws NullPointerException{
      	return null;
    
     }
